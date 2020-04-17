409789a539daa8604073ca956fb044f50be426dc
Madison <madifisher15@yahoo.com>
2020-04-17 01:01:07 -0500

    Saving data for WeatherPy

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "a528e8ab0a688f72975c10df8621f5a8"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyA4Pk3M9iUoHbLObVo_Qj6MLblsTUE6uU0"
